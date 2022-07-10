e70ee9e759b00f0c0a67e8eb7910a426aba47c32
kimballcaroline <kimballcaroline@gmail.com>
2022-07-10 11:03:41 -0400

    Added footer and formatted header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Caroline Kimball and is open-sourced on{" "}
+        <a
+          href="https://github.com/kimballcaroline/week-5-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
