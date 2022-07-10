220e79decb1ca6fd57b4380e1246531597120358
kimballcaroline <kimballcaroline@gmail.com>
2022-07-10 11:05:36 -0400

    Updated footer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Caroline Kimball and is open-sourced on{" "}
+        This project was coded by Caroline Kimball and is{" "}
         <a
           href="https://github.com/kimballcaroline/week-5-react-weather-app"
           target="_blank"
           rel="noreferrer"
         >
-          Github
+          open-sourced on Github
         </a>
       </footer>
     </div>
