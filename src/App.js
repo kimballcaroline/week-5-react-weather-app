ba528656396abd73074231cc8d332c5ea62f66b8
kimballcaroline <kimballcaroline@gmail.com>
2022-07-10 11:16:13 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Caroline Kimball and is{" "}
-        <a
-          href="https://github.com/kimballcaroline/week-5-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Caroline Kimball and is{" "}
+          <a
+            href="https://github.com/kimballcaroline/week-5-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
